33e7a27a441c1ff1cb70e21f03e854dbea8aa476
adkelley <adkelley@mac.com>
2024-06-01 15:07:45 -0700

    Hello Route - Path Param

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use axum::{
-    extract::Query,
+    extract::{Path, Query},
     response::{Html, IntoResponse},
     routing::get,
     Router,
@@ -9,7 +9,9 @@ use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/hello", get(handler_hello));
+    let app = Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/:name", get(handler_hello2));
 
     // region:         ---Start Server
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
@@ -27,9 +29,16 @@ struct HelloParams {
     name: Option<String>,
 }
 
+// e.g., `/hello?name=Alex`
 async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
     println!("->> {:<12} - handler_hello - {params:?}", "HANDLER");
     let name = params.name.as_deref().unwrap_or("World");
     Html("Hello, <strong>{name}!!!</strong>")
+}
+
+// e.g., `/hello2/Mike`
+async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello2 - {name:?}", "HANDLER");
+    Html(format!("Hello2, <strong>{name}!!!</strong>"))
 }
 // endregion:      ---Handler Hello
