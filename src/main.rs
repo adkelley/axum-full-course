8a2ba01962f873957fe30442dbe1729aad0866b7
adkelley <adkelley@mac.com>
2024-06-01 14:45:33 -0700

    Quick_Dev

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,27 @@
-use axum::{response::Html, routing::get, Router};
+use axum::{
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route(
-        "/",
-        get(|| async { Html("Hello, <strong>world!!!</strong>") }),
-    );
+    let app = Router::new().route("/hello", get(handler_hello));
 
     // region:         ---Start Server
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
-    println!("listening on {}", addr);
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("->> Listening on {}", addr);
     axum_server::bind(addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
     // endregion:      ---Start Server
 }
+
+// region:         ---Handler Hello
+async fn handler_hello() -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello", "HANDLER");
+    Html("Hello, <strong>world!!!</strong>")
+}
+// endregion:      ---Handler Hello
