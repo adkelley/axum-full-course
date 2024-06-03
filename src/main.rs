58b51f30b517d7f99cbe2d7f5c07ccef4f024da6
adkelley <adkelley@mac.com>
2024-06-03 11:44:03 -0700

    Error Client & Server

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,16 @@ use axum::{
     middleware,
     response::{Html, IntoResponse, Response},
     routing::{get, get_service},
-    Router,
+    Json, Router,
 };
 use model::ModelController;
 use serde::Deserialize;
+use serde_json::json;
 use std::net::SocketAddr;
 use tokio::net::TcpListener;
 use tower_cookies::CookieManagerLayer;
 use tower_http::services::ServeDir;
+use uuid::Uuid;
 
 // region:         ---Modules
 mod ctx;
@@ -57,9 +59,34 @@ async fn main() -> Result<()> {
 // region:         ---Response Mapper
 async fn main_response_mapper(res: Response) -> Response {
     println!("->> {:<12} - main_response_mapper", "RES_MAPPER");
+    let uuid = Uuid::new_v4();
+
+    // -- Get the eventual response error
+    let service_error = res.extensions().get::<Error>();
+    let client_status_error = service_error.map(|se| se.client_status_and_error());
+
+    // -- If client error, build the new response.
+    let error_response = client_status_error
+        .as_ref()
+        .map(|(status_code, client_error)| {
+            let client_error_body = json!({
+                "error": {
+                    "type": client_error.as_ref(),
+                    "req_uuid": uuid.to_string(),
+                }
+            });
+
+            println!("    ->> client_error_body: {client_error_body}");
+
+            // Build the new response from the client body
+            (*status_code, Json(client_error_body)).into_response()
+        });
+
+    // -- TODO: Build and log the server log line
+    println!("    ->> server log line - {uuid} - Error: {service_error:?}");
 
     println!();
-    res
+    error_response.unwrap_or(res)
 }
 // endregion:      ---Response Mapper
 
